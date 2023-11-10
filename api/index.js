08174c9b3390f79ccdd2d99b71d58dd145db4981
Diti2604 <ditiboyboy@gmail.com>
2023-11-10 22:18:51 +0100

    database okay

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 
-
 mongoose.connect(process.env.MONGO).then(() =>{
   console.log('Connected to mongodb')
 }).catch((err) => {
